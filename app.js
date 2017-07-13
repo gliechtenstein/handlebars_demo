e3c085fe8ea34c23fe8db5c785f6162951878c8f
gliechtenstein <ethan.gliechtenstein@gmail.com>
2017-07-13 18:33:02 -0400

    create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+ಠᴥಠ = {
+  $cell: true,
+  style: "padding: 30px;",
+  $components: [
+    { $components: [media(data), media(data)] }, 
+    { class: "row", $components: [card(data), card(data)] }
+  ]
+} 
