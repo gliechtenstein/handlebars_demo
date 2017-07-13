2fe5f381cc1b70588ee956580441d86e9a3599cb
gliechtenstein <ethan.gliechtenstein@gmail.com>
2017-07-13 18:32:51 -0400

    create data.js

---
+++ data.js
@@ -0,0 +1,5 @@
+var data = {
+  image: "https://png.icons8.com/notification/ios7/200",
+  title: "Media heading",
+  content: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus."  
+}
